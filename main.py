91a785cc626ba089a100afb9708394ce8f51349d
John Janecek <janecektyler@gmail.com>
2021-04-25 17:45:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ def getDate():
     return 'Updated at: ' + date.strftime(date_format)
 
 def callPlayers():
-    playersRes = requests.get("https://tapi.socomcommunity.com/api/universes/players?applicationId=10684")
+    playersRes = requests.get("http://s2online.socomcommunity.com/api/universes/players?applicationId=10684")
     info = playersRes.json()
     return info
 
 def callGames():
-    gameRes = requests.get("https://tapi.socomcommunity.com/api/rooms/68")
+    gameRes = requests.get("http://s2online.socomcommunity.com/api/rooms/68")
     gameInfo = gameRes.json()
     return gameInfo
 
